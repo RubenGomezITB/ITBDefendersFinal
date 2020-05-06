6dd3f50963744f822d5d15619c47c5484d22eb94
Ruben <ruben.gomez.7e2@itb.cat>
2020-05-06 18:58:04 +0200

    tracking

---
+++ BuildersDefendersFinal/Assets/Scripts/UnitAttack/UnitAttackDetecter.cs
@@ -0,0 +1,48 @@
+﻿﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitAttackDetecter : MonoBehaviour
+{
+    private BoxCollider _boxCollider;
+
+    public bool isOnAttackRange = false;
+
+    public UnitDetectRange DetectRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _boxCollider = GetComponent<BoxCollider>();
+   
+    }
+
+    private void FixedUpdate()
+    {
+        if (DetectRange.isOnDetectRange && !isOnAttackRange)
+        {
+            transform.parent.position = Vector3.MoveTowards(transform.parent.position,DetectRange.target.transform.position, .01f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (DetectRange.isOnDetectRange && other.tag == (DetectRange.DefenseTag))
+        {
+            isOnAttackRange = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (DetectRange.isOnDetectRange && other.tag ==(DetectRange.DefenseTag))
+        {
+            //attack
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isOnAttackRange = false;
+    }
+}
---
+++ BuildersDefendersFinal/Assets/Scripts/UnitsMovement/Movement.cs
@@ -0,0 +1,41 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject nexus;
+
+    public float velocity = 5;
+
+    public NavMeshAgent agent;
+
+    private UnitDetectRange _unitAttackRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity *= Time.deltaTime;
+        _unitAttackRange = GetComponentInChildren<UnitDetectRange>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_unitAttackRange.isOnDetectRange)
+        {
+            agent.isStopped = true;
+            /*if (expr)
+            {
+                
+            }*/
+        }
+        else
+        { 
+            agent.isStopped = false;
+            agent.SetDestination(nexus.transform.position);
+        }
+
+    }
+}
