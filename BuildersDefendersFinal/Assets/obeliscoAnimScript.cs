9d204bd70409b44a4b23f74ae268ff70c6fedd76
kleyton <kleyton@hotmail.es>
2020-05-21 22:16:29 +0200

    HUD version movil + modelo del gladidor sin animar Falta acabar la pestaña home y la configuracion del obelisco

---
+++ BuildersDefendersFinal/Assets/obeliscoAnimScript.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obeliscoAnimScript : MonoBehaviour
+{
+    public Animator animator;
+    public UnitDie unitDie;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        unitDie = GetComponent<UnitDie>();
+        unitDie.OnDefenseUnitDied += onUnitDied;
+    }
+
+    private void onUnitDied(object sender, EventArgs e)
+    {
+        animator.SetBool("morir",true);
+    }
+
+}
