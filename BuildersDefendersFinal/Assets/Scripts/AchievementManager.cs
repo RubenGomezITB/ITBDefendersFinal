873585f3c5b8814368189596cb0b850c27e73ead
Ruben <ruben.gomez.7e2@itb.cat>
2020-05-27 17:15:33 +0200

    Done

--- BuildersDefendersFinal/Assets/Scripts/AchievementManager.cs
+++ BuildersDefendersFinal/Assets/Scripts/AchievementManager.cs
@@ -1,32 +1,45 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CloudOnce;
 
 public class AchievementManager : MonoBehaviour
 {
+    [SerializeField] public int winGames;
 
-    [SerializeField]
-    public int winGames;
     // Start is called before the first frame update
     void Start()
     {
-        Cloud.OnInitializeComplete += CloudOnceInitializeComplete;
+        Cloud.OnInitializeComplete += CloudInitializeComplete;
         Cloud.Initialize(false, true);
     }
 
-    public void CloudInitializeComplete(){
+    public void CloudInitializeComplete()
+    {
         Debug.Log("Initialize");
     }
 
-    public void OnGameWin(){
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            OnGameWin();
+        }
+    }
+
+    public void OnGameWin()
+    {
         winGames += 1;
-        if(winGames == 1){
-            Achivements.Beginner.Unlock();
+        if (winGames == 1)
+        {
+            Achievements.Begginer.Unlock();
             Debug.Log("Begg");
-        } else if (winGames == 3){
-            Achivements.Amateur.Unlock();
+        }
+        else if (winGames == 3)
+        {
+            Achievements.Amateur.Unlock();
             Debug.Log("Amat");
-        } 
+        }
     }
 }
